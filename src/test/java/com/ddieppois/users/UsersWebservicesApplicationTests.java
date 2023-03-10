56e893518acd17e94ac4d5a2c5026214da6f9ec1
David Dieppois <David.Dieppois@gmail.com>
2023-03-10 08:34:46 -0500

    UserWebservices added test

---
+++ src/test/java/com/ddieppois/users/UsersWebservicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ddieppois.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class UsersWebservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
